6cddb58f962796cda1d6cf88b074e3c60b1881f8
finalexp <ad_exp@hotmail.com>
2019-04-10 20:05:56 +0200

    sp to test project

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
  <body <?php body_class(); ?>>
        <header class="site-header">
     <div class="container">
-      <h1 class="school-logo-text float-left"><a href="<?php echo esc_url(site_url())?>"><strong>Swedish</strong> Paleo</a></h1>
+      <h1 class="school-logo-text float-left"><a href="<?php echo esc_url(site_url())?>"><strong>Test</strong> Project</a></h1>
       <a href="<?php echo esc_url(site_url('/sok')); ?>" class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
